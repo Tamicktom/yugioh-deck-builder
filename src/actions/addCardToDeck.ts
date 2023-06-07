86d3d12a8a773b60fcb4d2b58e626832fb4afc34
Tamicktom <rickpokemon38@gmail.com>
2023-06-07 16:34:09 -0300

    Update addCardToDeck.ts

--- src/actions/addCardToDeck.ts
+++ src/actions/addCardToDeck.ts
@@ -5,5 +5,12 @@ import firebase from "@/services/connectFirebase";
 import type { APIResponse, CardData } from "@/types/yugioh-api-response";
 
 export default async function addCardToDeck(card: CardData) {
-  console.log("addCardToDeck", card.id);
+  const userId = firebase.auth().currentUser?.uid;
+  if (!userId)
+    return {
+      error: true,
+      message: "User not logged in",
+    };
+
+  
 }
