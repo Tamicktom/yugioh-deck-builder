f829324691b1d4dd77d8c73276a7d8feb395d0b4
Tamicktom <rickpokemon38@gmail.com>
2023-06-10 14:07:56 -0300

    now can remove cards

--- src/actions/addCardToDeck.ts
+++ src/actions/addCardToDeck.ts
@@ -6,11 +6,32 @@ import type { APIResponse, CardData } from "@/types/yugioh-api-response";
 
 export default async function addCardToDeck(card: CardData) {
   const userId = firebase.auth().currentUser?.uid;
-  if (!userId)
+  if (!userId) {
+    console.log("user not logged in");
     return {
       error: true,
       message: "User not logged in",
     };
+  }
 
-  
+  try {
+    const userRef = firebase.firestore().collection("users").doc(userId);
+
+    // Obtenha o documento atual do usuário
+    const userDoc = await userRef.get();
+    const userData = userDoc.data();
+
+    if (userData && userData.cards) {
+      // Se já houver um array de cartas, adicione a nova carta a ele
+      const updatedCards = [...userData.cards, { id: card.id, name: card.name }];
+      await userRef.update({ cards: updatedCards });
+    } else {
+      // Se ainda não houver um array de cartas, crie um novo com a nova carta
+      await userRef.set({ cards: [{ id: card.id, name: card.name }] });
+    }
+
+    console.log("Sucesso");
+  } catch (error) {
+    console.error("Falha ao adicionar a carta ao deck:", error);
+  }
 }
--- src/actions/removeCardFromDeck.ts
+++ src/actions/removeCardFromDeck.ts
@@ -6,5 +6,41 @@ import firebase from "@/services/connectFirebase";
 import type { APIResponse, CardData } from "@/types/yugioh-api-response";
 
 export default async function removeCardFromDeck(card: CardData) {
-  console.log("removeCardFromDeck", card.id);
+  const userId = firebase.auth().currentUser?.uid;
+  if (!userId) {
+    console.log("User not logged in");
+    return {
+      error: true,
+      message: "User not logged in",
+    };
+  }
+
+  try {
+    const userRef = firebase.firestore().collection("users").doc(userId);
+
+    // Obtenha o documento atual do usuário
+    const userDoc = await userRef.get();
+    const userData = userDoc.data();
+
+    if (userData && userData.cards) {
+      // Verifique se a carta está presente no array de cartas
+      const cardIndex = userData.cards.findIndex(
+        (c: CardData) => c.id === card.id && c.name === card.name
+      );
+
+      if (cardIndex !== -1) {
+        // Se a carta estiver presente, remova-a do array
+        const updatedCards = [...userData.cards];
+        updatedCards.splice(cardIndex, 1);
+        await userRef.update({ cards: updatedCards });
+        console.log("Carta removida com sucesso");
+      } else {
+        console.log("A carta não está presente no deck");
+      }
+    } else {
+      console.log("O deck está vazio");
+    }
+  } catch (error) {
+    console.error("Falha ao remover a carta do deck:", error);
+  }
 }
