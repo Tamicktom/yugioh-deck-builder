36ed31c7feeee1ec76b295e4cb91d6bc01e9d9a5
Tamicktom <rickpokemon38@gmail.com>
2023-06-16 20:18:11 -0300

    update

--- src/actions/getCardFromDb.ts
+++ src/actions/getCardFromDb.ts
@@ -59,7 +59,7 @@ export default async function getCardFromDb(
 
     // if the document doesn't exist, fetch the card data from the api, add it to the database and return it
     const response = await fetch(
-      `https://db.ygoprodeck.com/api/v7/cardinfo.php?&num=12&offset=0&fname=${cardName}`
+      `https://db.ygoprodeck.com/api/v7/cardinfo.php?&num=12  &offset=0&fname=${cardName}`
     );
     const data: APIResponse = await response.json();
 
